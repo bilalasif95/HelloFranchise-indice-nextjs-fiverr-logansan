911291323f8b9d61257d7cc8df19b70a103de533
nathan shearer <help@rapidstartup.io>
2023-06-27 15:01:42 +0800

    update base

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://indice-dynamic.envytheme.com"
+		//? "https://indice-dynamic.envytheme.com"
+		? "https://hello-franchise-indice-nextjs.vercel.app"
 		: "http://localhost:3000";
 
 export default baseUrl;
