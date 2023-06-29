ec915151d6ea5169d95d55d8c502c8b0b62d81be
nathan shearer <help@rapidstartup.io>
2023-06-29 12:06:59 +0800

    change baseurl to new domain

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,8 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
 		// ? "https://indice-dynamic.envytheme.com"
-		? "https://hello-franchise-indice-nextjs-i4tm.vercel.app"
+		//? "https://hello-franchise-indice-nextjs-i4tm.vercel.app"
+		? "https://aidirectory.app"
 		: "http://localhost:3000";
 
 export default baseUrl;
