750ff511566a551794e788057346d49d231f9d23
nathan shearer <help@rapidstartup.io>
2023-07-08 21:31:05 +0800

    fix layout syntax

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -21,11 +21,15 @@ const Layout = ({ children }) => {
         />
         <title>HelloFranchise - Directory & Listing Of The Worlds Most Successful Franchises</title>
         <script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
-        <script>
-          Weglot.initialize({
-            api_key: 'wg_a80c0ace692c50a2b2367b9a32b6c61d0'
-          });
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              Weglot.initialize({
+                api_key: 'wg_a80c0ace692c50a2b2367b9a32b6c61d0'
+              });
+            `,
+          }}
+        />
       </Head>
 
       {children}
